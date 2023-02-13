4b21ad3c53fc55b6c68d5a9d6cd5092be12d6960
I am Alaska <stephen.alaska94@hotmail.com>
2023-02-13 13:57:19 +0000

    create book class

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-const booklists = [{ title: 'title1', author: 'author1' }, { title: 'title2', author: 'author2' }];
-
 const booklistContainer = document.querySelector('.book-list-container');
+const form = document.querySelector('form');
+
+const booklists = [
+  { title: 'title1', author: 'author1' },
+  { title: 'title2', author: 'author2' },
+];
+
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = form.title;
+  const author = form.author;
+  if (title.length && author.length) {
+    const newBook = new Book(title, author);
+    console.log(newBook);
+  }
+});
 
 function listAllBooks() {
   let booklistHtml = '';
